7409ec8f8953e62096481fed2d7a88ee782d79e9
LAPTOP-RQBHQ9EN/Kent Angelo <cabanagkentangelo@gmail.com>
2023-01-24 00:34:01 +0800

    Adding def bubble sort function

--- Bubble_Sort.py
+++ Bubble_Sort.py
@@ -1,1 +1,12 @@
 # Bubble Sort
+def bubble_sort(list):
+    var = len(list)
+
+    for i in range(var-1):
+        for j in range(var-1):
+            if list[j] > list[j+1]: 
+                tmp = list[j]
+                list[j] = list[j+1]
+                list[j+1] = tmp
+
+    return list
