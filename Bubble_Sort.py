df20063df05c608792b953ab5286208abd143bc1
LAPTOP-RQBHQ9EN/Kent Angelo <cabanagkentangelo@gmail.com>
2023-01-24 00:42:46 +0800

    Last commit in Bubble Sort function

--- Bubble_Sort.py
+++ Bubble_Sort.py
@@ -12,6 +12,8 @@ def bubble_sort(list):
     return list
 
 def main():
+    print("Array values: [93, 40, 91, 62, 56, 38, 46, 64, 78, 87]")
+    print("\nBubble Sort")
     list = [93, 40, 91, 62, 56, 38, 46, 64, 78, 87] # My Array values
     bubble_sort(list)
     print(list) # printing the selection sort
