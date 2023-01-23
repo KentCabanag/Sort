d94e5bd306a4f7cab17d3212ba1b3c848cbdfe14
LAPTOP-RQBHQ9EN/Kent Angelo <cabanagkentangelo@gmail.com>
2023-01-24 01:06:50 +0800

    Adding the def Insertion Sort function

--- Insertion_Sort.py
+++ Insertion_Sort.py
@@ -1,1 +1,13 @@
 # Insertion Sort
+def insertion_sort(list):  
+    for i in range(1, len(list)):  
+        current = list[i]  
+
+        j = i - 1  
+        while j >= 0 and current < list[j]:  
+            list[j+1] = list[j]  
+            j -= 1  
+        list[j+1] = current  
+
+    return list
+
