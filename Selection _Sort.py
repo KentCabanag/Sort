e04b58204fba11590a7e9aa02d30b9c6b8e73fb4
LAPTOP-RQBHQ9EN/Kent Angelo <cabanagkentangelo@gmail.com>
2023-01-23 23:34:11 +0800

    adding range

--- Selection _Sort.py
+++ Selection _Sort.py
@@ -1,4 +1,16 @@
 #Selection Sort
 def selection_sort(list):
     var = len(list)
+    for i in range(var-1): 
+        min = i
 
+        for j in range(i+1, var):
+            if list[j] < list[min]:
+                min = j
+
+        if min != i:
+            temp = list[i]
+            list[i] = list[min]
+            list[min] = temp
+
+    return list
