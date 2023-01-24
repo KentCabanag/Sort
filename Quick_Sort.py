342b4c0281460f34e1a6a53aecf823c132725d9d
LAPTOP-RQBHQ9EN/Kent Angelo <cabanagkentangelo@gmail.com>
2023-01-25 00:22:42 +0800

    applying partition

--- Quick_Sort.py
+++ Quick_Sort.py
@@ -1,1 +1,14 @@
 # Quick Sort
+
+def partition(list, low, high):
+    i = low
+    pivot = list[high]
+
+    for j in range(low, high):
+        if list[j] <= pivot:
+            list[i], list[j] =  list[j], list[i]
+            i += 1
+
+    list[i], list[high] = list[high] ,list[i]
+
+    return i
