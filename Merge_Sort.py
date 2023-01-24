0ca4658c370f37ac368cdda60ec081db9b925c76
LAPTOP-RQBHQ9EN/Kent Angelo <cabanagkentangelo@gmail.com>
2023-01-24 23:40:49 +0800

    Creating while

--- Merge_Sort.py
+++ Merge_Sort.py
@@ -14,4 +14,19 @@ def merge_sort(list):
     return merge(left, right)
     
 def merge(left, right):
-    
+    output = []
+    i = j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            output.append(left[i])
+            i += 1
+        else:
+            output.append(right[j])
+            j += 1
+
+    output.extend(left[i:])
+    output.extend(right[j:])
+
+    return output
+
