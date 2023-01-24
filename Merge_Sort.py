26e93e81f0c3152ebd8d76d9bcae026ac94ad932
LAPTOP-RQBHQ9EN/Kent Angelo <cabanagkentangelo@gmail.com>
2023-01-24 23:39:02 +0800

    Implementing Merge sort

--- Merge_Sort.py
+++ Merge_Sort.py
@@ -1,1 +1,17 @@
 # Merge Sort
+
+def merge_sort(list):
+    length = len(list)
+
+    if length == 1:
+        return list
+
+    mid = length // 2
+
+    left = merge_sort(list[:mid])
+    right = merge_sort(list[mid:])
+
+    return merge(left, right)
+    
+def merge(left, right):
+    
